a1a5708d64d21f39765116bf69b489b183eb337c
Sebastian H. <maracuchoamericano@gmail.com>
2025-08-04 12:31:53 -0400

    fixed the dev path to ssl

--- app.ts
+++ app.ts
@@ -96,8 +96,8 @@ if (process.env.NODE_ENV === "DEV") {
     // STEP 1: This will be where the certificates are stored.
 
     options = {
-        key: fs.readFileSync('C:\\Program Files\\Git\\usr\\bin\\key.pem'),
-        cert: fs.readFileSync('C:\\Program Files\\Git\\usr\\bin\\certificate.pem'),
+        key: fs.readFileSync("/etc/letsencrypt/live/api.toomanyheys.com/privkey.pem"),
+        cert: fs.readFileSync('/etc/letsencrypt/live/api.toomanyheys.com/fullchain.pem'),
         // Remove this line once done with production
         rejectUnauthorized: false
     };    
